97728f7f4c45e8401019cfe0fd42d9ee7873c6e9
greenwormstudio <greenwormstudio@gmail.com>
2025-08-27 11:20:13 +0700

    demo function

--- C/function/menu.c
+++ C/function/menu.c
@@ -17,6 +17,33 @@ int summary(int a, int b)
     total = a + b; // 3 + 5 = 8
     return total;
 }
+// Định nghĩa hàm trừ hai số a và b
+int sub(int a, int b){
+    int result;
+    result = a - b;
+    return result;
+}
+// Định nghĩa hàm nhân 2 số a và b
+int multiple(int a, int b){
+    int result;
+    result = a * b;
+    return result;
+}
+// Định nghĩa phép chia 2 số.
+double div(int a, int b){
+    double result;
+    if (b!=0)
+    {
+      result = a/b;
+    }
+    else
+    {
+        result = 0;
+        printf("Can not div by zero \n");
+    }
+    return result;
+    
+}
 int main()
 {
     int  choice =0;
@@ -27,6 +54,7 @@ int main()
     { 
         printf("Please enter your choice (1-9) \n");
         scanf("%d",&choice);
+        printf("Enter a and b \n");
         scanf("%d %d",&a,&b);
         switch (choice)
         {
@@ -38,12 +66,21 @@ int main()
             break;
         case 2: 
             printf("You chose option 2 Subtract 2 numbers \n");
+            // Gọi hàm trừ 2 số
+            result = sub(a,b);
+            printf("Subtraction: %d\n", result);
             break;
         case 3:
             printf("You chose option 3 Multiply 2 numbers \n");
+            // Gọi hàm nhân 2 số
+            result = multiple(a,b);
+            printf("Multiplication: %d\n", result);
             break;
         case 4:
             printf("You chose option 4 Divide 2 numbers \n");
+            // Gọi hàm chia 2 số
+            double divResult = div(a,b);
+            printf("Division = %f\n", divResult);
             break;
         case 9:
             printf("You chose option 9\n. Exit");
