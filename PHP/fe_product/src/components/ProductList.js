8a7afa1b762f98c4da5472859f30f8b75e3b9a53
NguyenCuong32 <cuongnx32@fpt.edu.vn>
2025-12-08 09:37:15 +0700

    fix: Correct useEffect dependency array in ProductList component

--- PHP/fe_product/src/components/ProductList.js
+++ PHP/fe_product/src/components/ProductList.js
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import axios from "axios";
 function ProductList() {
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/products").then((response) => {
+            console.log(response.data);
+        }).catch((error) => {
+            console.error("There was an error fetching the products!", error);
+        });
+        
+    })
     return (
         <div>
             <h1>Product List</h1>
+            
             {/* Product items will be rendered here */}
         </div>
     );
